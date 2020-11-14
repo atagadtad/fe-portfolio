b46f8924a55e99775f30f8d25abf209aab324983
Aaron Tagadtad <aaron@alias.com>
2020-11-14 00:51:08 -0500

    basic responsive design in courses view

--- src/views/Courses/Courses.js
+++ src/views/Courses/Courses.js
@@ -1,9 +1,9 @@
 const Courses = () => {
   return (
-    <div className="bg-gray-700 h-full">
-      <div className="grid grid-cols-3 h-full">
-        <div className="bg-gray-400 col-span-1 h-full"></div>
-        <div className="bg-gray-600 h-64 col-span-2"></div>
+    <div className="container sm: mx-auto min-w-full h-full">
+      <div className="grid grid-cols-2 md:grid-cols-3 h-full">
+        <div className="bg-gray-400 col-span-2 md:col-span-1 h-full"></div>
+        <div className="bg-gray-600 col-span-2 md:col-span-2 h-full"></div>
       </div>
     </div>
   );
